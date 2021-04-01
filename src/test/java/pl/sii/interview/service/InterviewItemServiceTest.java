cb538b9b0bdb3ac9169d25ac4639ebf2fb764c7a
abudzynski <abudzynski@pl.sii.eu>
2021-04-01 17:04:51 +0200

    [PRACTICE] - arrayItems_toMap_uniqueEntries

--- src/test/java/pl/sii/interview/service/InterviewItemServiceTest.java
+++ src/test/java/pl/sii/interview/service/InterviewItemServiceTest.java
@@ -3,12 +3,15 @@ package pl.sii.interview.service;
 import org.junit.jupiter.api.Test;
 import pl.sii.interview.demo.model.InterviewItem;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static java.util.Arrays.stream;
 import static java.util.Collections.emptyList;
+import static java.util.Collections.emptyMap;
+import static java.util.stream.Collectors.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -52,13 +55,43 @@ class InterviewItemServiceTest {
                 .filter(item -> Optional.ofNullable(item.getAnswer()).map(answer -> answer.contains("y")).orElse(false))
                 .filter(item -> item.getQuestion().endsWith("?"))
                 .filter(item -> !item.getQuestion().endsWith("??"))
-                .collect(Collectors.toList());
+                .collect(toList());
         /* </ANSWER> */
 
         assertThat(resultList).hasSize(3);
         assertThat(resultList).contains(InterviewItem.builder().question("Abc ?").answer("Xyz").build());
         assertThat(resultList).contains(InterviewItem.builder().question("Is this real?").answer("Maybe Yes").build());
         assertThat(resultList).contains(InterviewItem.builder().question("Is this real?").answer("Maybe No").build());
+    }
+
+    @Test
+    void arrayItems_toMap_uniqueEntries() {
+        Map<String, ? extends Collection<String>> resultMap = emptyMap();
+
+        /* TODO from the interviewItemsArray create a map {key: question, answers: [answer1, answer2]}
+           - questions should be unique
+           - answers to the questions should be unique
+        */
+
+        /* <ANSWER> */
+        resultMap = stream(interviewItemsArray)
+                .collect(groupingBy(
+                        InterviewItem::getQuestion,
+                        mapping(InterviewItem::getAnswer, toSet())
+                ));
+        /* </ANSWER> */
+
+        assertThat(resultMap).hasSize(3);
+        assertThat(resultMap).containsKeys("Abc ?", "Abc ??", "Is this real?");
+        assertThat(resultMap.get("Abc ?"))
+                .hasSize(3)
+                .contains("Xyz", "Good", null);
+        assertThat(resultMap.get("Abc ??"))
+                .hasSize(1)
+                .contains("Maybe C?");
+        assertThat(resultMap.get("Is this real?"))
+                .hasSize(2)
+                .contains("Maybe Yes", "Maybe No");
     }
 
 }
